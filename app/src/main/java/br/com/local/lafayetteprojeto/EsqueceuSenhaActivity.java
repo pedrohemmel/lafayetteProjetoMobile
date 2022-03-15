ec261be4bc58e1de4715545c71bca900cfbebe7f
pedrohemmel <phenrique_h2016@hotmail.com>
2022-03-15 08:21:08 -0300

    first commit

---
+++ app/src/main/java/br/com/local/lafayetteprojeto/EsqueceuSenhaActivity.java
@@ -0,0 +1,14 @@
+package br.com.local.lafayetteprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EsqueceuSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.esqueceu_senha_layout);
+    }
+}
---
+++ app/src/main/java/br/com/local/lafayetteprojeto/ManageHomeActivity.java
@@ -0,0 +1,14 @@
+package br.com.local.lafayetteprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage_home_layout);
+    }
+}
