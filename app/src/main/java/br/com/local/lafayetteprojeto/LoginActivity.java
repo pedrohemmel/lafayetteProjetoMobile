b8acd37e9e3bbb737c1e1bc687362e25b5e6b2ed
pedrohemmel <phenrique_h2016@hotmail.com>
2022-03-15 11:39:10 -0300

    desenvolvimento de cards e galeria

--- app/src/main/java/br/com/local/lafayetteprojeto/LoginActivity.java
+++ app/src/main/java/br/com/local/lafayetteprojeto/LoginActivity.java
@@ -2,7 +2,10 @@ package br.com.local.lafayetteprojeto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        //criando botão e referenciando o mesmo
+        Button button = findViewById(R.id.btnEntrar);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainManageActivity.class));
+                finish();
+            }
+        });
 
     }
 }
--- app/src/main/java/br/com/local/lafayetteprojeto/MainManageActivity.java
+++ app/src/main/java/br/com/local/lafayetteprojeto/MainManageActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ManageHomeActivity extends AppCompatActivity {
+public class MainManageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.manage_home_layout);
+        setContentView(R.layout.main_manage_layout);
     }
 }
