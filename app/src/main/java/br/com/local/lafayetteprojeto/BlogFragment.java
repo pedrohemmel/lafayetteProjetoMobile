fcb7e31f5434a2902bed5b75bbbb5d4ca5d0c989
pedrohemmel <phenrique_h2016@hotmail.com>
2022-03-23 11:52:01 -0300

    new commit

--- app/src/main/java/br/com/local/lafayetteprojeto/BlogFragment.java
+++ app/src/main/java/br/com/local/lafayetteprojeto/BlogFragment.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+
+import java.util.List;
 
 public class BlogFragment extends Fragment {
 
+    List<Artigos> lstArtigos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,11 +21,12 @@ public class BlogFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_blog, container, false);
 
-
+        AdapterBlog mAdapterBlog = findViewById(R.id.recyclerViewBlog);
 
 
 
 
         return view;
     }
+
 }
