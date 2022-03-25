c78e94a850855d70ee12447b23e1ce7cd1db8769
pedrohemmel <phenrique_h2016@hotmail.com>
2022-03-25 11:45:06 -0300

    new commit

--- app/src/main/java/br/com/local/lafayetteprojeto/BlogFragment.java
+++ app/src/main/java/br/com/local/lafayetteprojeto/BlogFragment.java
@@ -3,12 +3,14 @@ package br.com.local.lafayetteprojeto;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlogFragment extends Fragment {
@@ -21,7 +23,11 @@ public class BlogFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_blog, container, false);
 
-        AdapterBlog mAdapterBlog = findViewById(R.id.recyclerViewBlog);
+        lstArtigos = new ArrayList<>();
+
+        lstArtigos.add(new Artigos("Pedro", "Henrique", "O brabo"));
+
+        RecyclerView mAdapterBlog = findViewById(R.id.recyclerViewBlog);
 
 
 
